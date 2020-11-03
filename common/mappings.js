42bd0a0ff9863ac5e2a83d99637fdffb1bd4d2af
Ali Karpuzoglu <ali.karpuzoglu@gmail.com>
2020-11-03 15:19:29 +0100

    added ip config on top and dynamic generation fix

--- common/mappings.js
+++ common/mappings.js
@@ -1,4 +1,26 @@
 var mappings = {
+  default: {
+    'vive-controls': {
+      trackpaddown: 'teleport'
+    },
+
+    'oculus-touch-controls': {
+      xbuttondown: 'teleport'
+    }
+  },
+  paint: {
+    common: {
+      triggerdown: 'paint'
+    },
+  
+    'vive-controls': {
+      menudown: 'toggleMenu'
+    },
+
+    'oculus-touch-controls': {
+      abuttondown: 'toggleMenu'
+    }
+  },
   mappings: {
     default: {
       common: {
@@ -18,3 +40,22 @@ var mappings = {
 };
 
 AFRAME.registerInputMappings(mappings);
+// var inputActions = {
+//   task1: {
+//     changeTask: { label: 'Change task' },
+//     logdefault: { label: 'Test Log' },
+//     logtask1: { label: 'Test Log Task 1' },
+//     logtask2: { label: 'Test Log Task 2' },
+//     lefthand: { label: 'Left hand' },
+//     righthand: { label: 'Right hand' },
+//     longpress: { label: 'Long press' },
+//     doubletouch: { label: 'Double touch' },
+//     doublepress: { label: 'Double press' }
+//   },
+//   task2: {
+//     changeTask: { label: 'Change task' },
+//     logtask2: { label: 'Test Log Task 2' }
+//   }
+// }
+
+// AFRAME.registerInputActions(inputActions, 'task1');
