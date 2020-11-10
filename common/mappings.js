bf17a1e2eca2b18c09589b7ec3e727c27b432d12
Ali Karpuzoglu <ali.karpuzoglu@gmail.com>
2020-11-10 18:53:40 +0100

    added the openMenu function and finished merging

--- common/mappings.js
+++ common/mappings.js
@@ -33,8 +33,9 @@ var mappings = {
     },
     'vive-controls': {
       'grip.down': 'changeTask',
-      'trackpad.down': 'logtask1',
-      'trackpad.doubletouch': 'doubletouch',
+      'trackpad.up': 'teleportend',
+      'trackpad.down': 'teleportstart',
+      'trackpad.doubletouch': 'openMenu',
       'trackpad.doublepress': 'doublepress',
       // Activators for down, up, touchstart and touchend are optionals you can just write the event without the .
       'trackpaddpadleftdown': 'dpadleft',
@@ -47,26 +48,13 @@ var mappings = {
       'grip.down': 'changeTask'
     },
     keyboard: {
-      't_up': 'logdefault',
-      'c_up': 'changeTask'
-    }
-  },
+      't_down': 'teleportstart',
+      't_up': 'teleportend',
+      'c_up': 'changeTask',
+      'm_up': 'openMenu'
 
-    default: {
-      common: {
-        trackpaddown: 'teleportstart',
-        trackpadup: 'teleportend'
-      },
-      'oculus-touch-controls': {
-        thumbstickdown: 'teleportstart',
-        thumbstickup: 'teleportend'
-      },
-      keyboard: {
-        't_down': 'teleportstart',
-        't_up': 'teleportend',
-        'm_up': 'openMenu',
-      }
     }
+  }
   }
 };
 
@@ -76,16 +64,16 @@ var inputActions = {
     openMenu: { label : 'Open Menu'},
     changeTask: { label: 'Change task' },
     logdefault: { label: 'Test Log' },
-    logtask1: { label: 'Test Log Task 1' },
+    logtask1: { label: 'Test Log Task 1' }, 
     logtask2: { label: 'Test Log Task 2' },
-    lefthand: { label: 'Left hand' },
-    righthand: { label: 'Right hand' },
+    lefthand: { label: 'Left hand' },// the trigger on the back
+    righthand: { label: 'Right hand' }, // the trigger on the back
     longpress: { label: 'Long press' },
     doubletouch: { label: 'Double touch' },
     doublepress: { label: 'Double press' }
   },
-  task2: {
-    changeTask: { label: 'Change task' },
+  controls: {
+    openMenu: { label : 'openMenu'},
     logtask2: { label: 'Test Log Task 2' }
   }
 }
